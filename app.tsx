6220535f056c3a01397b5b0f94eb4b820de6a1ff
Ved Iyer <158789105+vediy@users.noreply.github.com>
2025-04-20 11:02:14 -0500

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,18 @@
+// "use client";
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './page';         // page.tsx
+// import Selection from './selection/page';       // selection.tsx
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/page" element={<Home />} />
+//         <Route path="/selection" element={<Selection />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
---
+++ page.tsx
@@ -0,0 +1,93 @@
+'use client';
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[16px] row-start-2 items-center sm:items-start">
+        <Image
+          className="image mx-auto"
+          src="/careconnect.svg"
+          alt="CareConnect logo"
+          width={250}
+          height={60}
+          priority
+        />
+        <ol className="text-sm/6 text-center sm:text-center text mx-auto font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Connecting people who can help with the right resources
+          </li>
+          <li className="tracking-[-.01em]">
+            Click below to begin caring for your community!
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row justify-center mx-auto w-fit">
+          <Link
+            href="/selection"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Start Now
+          </Link>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
